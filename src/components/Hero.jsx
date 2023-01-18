dc58a2e2567a23b06c0717a16c8f39135caba436
Nermeen-M <nermeen.mamdouh.nm@gmail.com>
2023-01-18 14:15:15 +0100

    Hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+export default function Hero() {
+  return (
+    <section id="hero">
+      <div className="text">
+        <h1>Vegan Foods</h1>
+        <h2>Fresh and Natural</h2>
+      </div>
+    </section>
+  );
+}
